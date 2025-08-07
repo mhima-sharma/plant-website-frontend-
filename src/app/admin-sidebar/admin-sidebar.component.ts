01f30243394b42e4d7f573f385dc50c2779dcfe1
mahima-sharma <sharmamahima0510@gmail.com>
2025-08-07 14:05:44 +0530

    sidebar

--- src/app/admin-sidebar/admin-sidebar.component.ts
+++ src/app/admin-sidebar/admin-sidebar.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-sidebar.component.html',
   styleUrl: './admin-sidebar.component.css'
 })
 export class AdminSidebarComponent {
+ isSidebarOpen = false;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
