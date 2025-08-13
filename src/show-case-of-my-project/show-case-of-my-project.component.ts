d8511ad1ae0c9b8633b4d2697144e802202a3085
mahima-sharma <sharmamahima0510@gmail.com>
2025-08-13 10:13:50 +0530

    ui changes

--- src/show-case-of-my-project/show-case-of-my-project.component.ts
+++ src/show-case-of-my-project/show-case-of-my-project.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { HeaderComponent } from "../elements/header/header.component";
 import { ContentComponent } from "../elements/content/content.component";
 import { FooterComponent } from "../elements/footer/footer.component";
@@ -9,28 +10,80 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-show-case-of-my-project',
-  imports: [HeaderComponent, ContentComponent, FooterComponent, NeedHelpComponent, PlantsProductComponent, CommonModule],
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    NeedHelpComponent,
+    PlantsProductComponent,
+    CommonModule,
+    ChatbotComponent
+  ],
   templateUrl: './show-case-of-my-project.component.html',
   styleUrl: './show-case-of-my-project.component.css'
 })
 export class ShowCaseOfMyProjectComponent {
-isChatbotOpen = false;
-isWeatherOpen = false;
- isGameOpen = false;
- selectedGame = 'tic-tac-toe';
+  activePanel: 'weather' | 'games' | 'chatbot' | null = null;
+  locationGranted = false;
+  userLocation: { lat: number; lon: number } | null = null;
 
- 
- toggleChatbot() {
-    this.isChatbotOpen = !this.isChatbotOpen;
-  }
-  toggleWeather() {
-  this.isWeatherOpen = !this.isWeatherOpen;
-}
- toggleGame() {
-    this.isGameOpen = !this.isGameOpen;
-  }
-  selectGame(game: string) {
-  this.selectedGame = game;
-}
+  selectedGame: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    // Load Tic Tac Toe by default
+    this.selectedGame = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'https://mhima-sharma.github.io/-tick-tack-/'
+    );
+  }
+
+  closePanel() {
+    this.activePanel = null;
+  }
+
+  openPanel(panel: 'weather' | 'games' | 'chatbot') {
+    if (panel === 'weather') {
+      this.requestLocationAccess();
+    }
+    this.activePanel = this.activePanel === panel ? null : panel;
+  }
+
+  selectGame(url: string) {
+    this.selectedGame = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  private requestLocationAccess() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.locationGranted = true;
+          this.userLocation = {
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude
+          };
+        },
+        () => {
+          this.locationGranted = false;
+        }
+      );
+    }
+  }
+
+  requestWeatherAccess() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        () => {
+          this.locationGranted = true;
+          this.openPanel('weather');
+        },
+        () => {
+          this.locationGranted = false;
+          this.openPanel('weather');
+        }
+      );
+    } else {
+      this.locationGranted = false;
+      this.openPanel('weather');
+    }
+  }
 }
